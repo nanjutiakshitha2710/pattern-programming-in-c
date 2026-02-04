7fbfc37d2ffc55b780b056982eb5b13bac91d700
nanjutiakshitha2710 <nanjutiakshitha@gmail.com>
2026-02-04 12:42:01 +0530

    Add files via upload

---
+++ square pattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+    int rows;
+    int cols;
+    printf("enter the number of rows\n");
+    scanf("%d",&rows);
+    printf("enter the number of cols\n");
+    scanf("%d",&cols);
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1;j<=cols;j++)
+        {
+            if(i==1||i==rows||j==1||j==cols)
+        {
+            printf("* ");
+        }
+        
+        else
+        {
+        printf("  ");
+    }
+}
+printf("\n");
+}
+}
